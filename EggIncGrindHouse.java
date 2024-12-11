89c273b10a00cebfe782ecc7508c6d10203d3b9c
Koolkidronnie <788199@dpsk12.net>
2024-12-10 17:44:06 -0700

    Update EggIncGrindHouse.java

--- EggIncGrindHouse.java
+++ EggIncGrindHouse.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous
-
+// THIS IS FOR WHEN WE ARE BLUE TEAM!!
 public class EggIncGrindHouse {
 
 	// todo: write your code here
 	// ADD SENSORS PLS PSP PSPL awgiho jioNOW NOWI NOWW
-
+	private detectObject()
 	
 }
